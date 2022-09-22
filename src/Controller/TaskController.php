8eea2b5cd25b6044c51764d8e2b9f414a8ec6680
arnaud.boubli <webmaster@jetunoo.fr>
2022-09-22 19:27:20 +0200

    install webpack, task controller and vue

--- src/Controller/TaskController.php
+++ src/Controller/TaskController.php
@@ -2,17 +2,19 @@
 
 namespace App\Controller;
 
+use App\Repository\TaskRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class TaskController extends AbstractController
 {
-    #[Route('/task', name: 'app_task')]
-    public function index(): Response
+    #[Route('/', name: 'home')]
+    public function index(TaskRepository $taskRepository): Response
     {
+        $listTask = $taskRepository->findAll();
         return $this->render('task/index.html.twig', [
-            'controller_name' => 'TaskController',
+            'listTask' => $listTask
         ]);
     }
 }
