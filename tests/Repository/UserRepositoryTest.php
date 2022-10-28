41fe6b024d928fbb22dca1f396330668b5b701cc
arnaud.boubli <webmaster@jetunoo.fr>
2022-10-28 18:21:50 +0200

    test UserRepository

---
+++ tests/Repository/UserRepositoryTest.php
@@ -0,0 +1,39 @@
+<?php 
+
+namespace App\Tests\Repository;
+
+
+use App\Entity\User;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+
+
+class UserRepositoryTest extends KernelTestCase
+{
+
+    /**
+     * @var \Doctrine\ORM\EntityManager
+     */
+    private $entityManager;
+    
+    
+    public function setUp(): void
+    {
+        parent::setUp();
+        $kernel = self::bootKernel();
+
+        $this->entityManager = $kernel->getContainer()
+            ->get('doctrine')
+            ->getManager();
+        
+    }
+
+    public function testSearchByUsername()
+    {
+        $user = $this->entityManager
+            ->getRepository(User::class)
+            ->findOneBy(['username' => 'Thomas'])
+        ;
+
+        $this->assertSame('Thomas', $user->getUsername());
+    }
+}
